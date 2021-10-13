ea89f0362077dd7ce2c64fb0a20303234b3a3ac2
Kim Dong In <needshield@gmail.com>
2021-10-13 14:42:55 +0900

    apply lombok

--- src/main/java/com/owljoa/myspringbatch/Person.java
+++ src/main/java/com/owljoa/myspringbatch/Person.java
@@ -1,39 +1,18 @@
 package com.owljoa.myspringbatch;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Person {
 
   private String lastName;
   private String firstName;
-
-  public Person() {
-  }
-
-  public Person(String lastName, String firstName) {
-    this.lastName = lastName;
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  @Override
-  public String toString() {
-    return "Person{" +
-        "lastName='" + lastName + '\'' +
-        ", firstName='" + firstName + '\'' +
-        '}';
-  }
 }
