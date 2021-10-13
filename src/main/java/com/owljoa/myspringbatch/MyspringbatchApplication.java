ec222851a9587be9c84df7a5ab9a694562271c40
Kim Dong In <needshield@gmail.com>
2021-10-13 10:48:55 +0900

    init project

---
+++ src/main/java/com/owljoa/myspringbatch/MyspringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.owljoa.myspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/owljoa/myspringbatch/MyspringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owljoa.myspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
