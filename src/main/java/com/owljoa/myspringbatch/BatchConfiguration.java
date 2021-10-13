a7c4aab1777ca001be7859492de1abbfdb305c24
Kim Dong In <needshield@gmail.com>
2021-10-13 13:47:12 +0900

    add job and step1 definitions

--- src/main/java/com/owljoa/myspringbatch/BatchConfiguration.java
+++ src/main/java/com/owljoa/myspringbatch/BatchConfiguration.java
@@ -1,9 +1,12 @@
 package com.owljoa.myspringbatch;
 
 import javax.sql.DataSource;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -53,5 +56,24 @@ public class BatchConfiguration {
         .build();
   }
 
+  @Bean
+  public Job importUserJob(JobCompletionNotificationListener listner, Step step1) {
+    return jobBuilderFactory.get("importUserJob")
+        .incrementer(new RunIdIncrementer())
+        .listener(listner)
+        .flow(step1)
+        .end()
+        .build();
+  }
+
+  @Bean
+  public Step step1(JdbcBatchItemWriter<Person> writer) {
+    return stepBuilderFactory.get("step1")
+        .<Person, Person>chunk(10)
+        .reader(reader())
+        .processor(processor())
+        .writer(writer)
+        .build();
+  }
 
 }
